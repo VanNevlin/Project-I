471c28119cc2218c9d3ac6a81658bc55c5dcba7e
shadowprince10 <belinda.jiahong@gmail.com>
2020-12-24 08:08:19 +0700

    message

--- recover_deleted_notes.cpp
+++ recover_deleted_notes.cpp
@@ -9,24 +9,56 @@
 struct user {
     char userName[255];
     char passWord[255];
-    char notes[255];
+    char notes[255][255];
     bool privat;
     user *Friend;
     user *prev, *next;
-    char deleted_notes;
-}*head = NULL, *tail = NULL, User;
+    char deleted_notes[255];
+}*head = NULL, *tail = NULL;
 
-void delete_notes()
+void delete_notes(user* current)
 {
-    struct user User;
-    char delete_note[255];
-    struct user User[1000];
-    scanf("%s", delete_note);
-    puts("Coming Soon!");
+    int index_del;
+    // post(); // untuk memanggil fungsi post notes dari @KFC
+    
+    // DEBUG
+    // printf("%d", &index); // untuk memastikan indeks dari fungsi KFC
 
+    // Ini untuk scan indeks yang ingin di-delete di file
+    scanf("%d", &index_del);
+    
+    for (int i = 0; i < index; i++)
+    {
+        for (int j = i; j < index; j++)  
+        {
+            if (strcmp(current -> notes[i][j], current -> notes[i][index_del]) == 0)
+            {
+                strcpy(current -> notes[i][index_del], current -> deleted_notes);
+                current -> notes[i][index_del] = NULL; // untuk menghapus notes
+            }
+        }
+    }
 }
 
-void recover_deleted_notes()
+void recover_deleted_notes(user *current)
 {
-    puts("Coming Soon!");        
+    int index_recover;
+
+    // post(); // untuk memanggil fungsi post notes dari @KFC
+    
+    // DEBUG
+    // printf("%d", &index); // untuk memastikan indeks dari fungsi KFC
+
+    scanf("%d", &index_recover);
+
+    for (int i = 0; i < index; i++)
+    {
+        for (int j = i; j < index; j++)  
+        {
+            if (strcmp(current -> deleted_notes, current -> notes[i][index_recover]) == 0)
+            {
+                strcpy(current -> notes[i][index_recover], current -> deleted_notes);
+            }
+        }
+    }
 }
